4462940f66411e5639ffe4464a5d98a925918662
1ilsang <1ilsang@naver.com>
2019-04-09 12:56:58 +0900

    FRONT] ADD login modal, MODULE, TYPO

--- WebContent/assets/js/Login.js
+++ WebContent/assets/js/Login.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('login-btn').addEventListener('click', e => {
         modalClear();
         if (isLogin('JSESSIONID')) {
-            // TODO Cookie 까서 확인. + 조회 // 탈퇴, 수정 fetch
+            // TODO 탈퇴, 수정 fetch
             if (confirm('Logout?')) {
                 fetch(`${LOCAL_HOST}/user.do?action=logout`).then(res => {
-                    if(res.status === 200) document.getElementById('login-btn').innerText = 'Login';
+                    if (res.status === 200) document.getElementById('login-btn').innerText = 'Login';
                 });
                 return;
             }
@@ -13,19 +13,43 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         let modalTmp = document.getElementById('open-modal-tmp');
         document.getElementById('open-modal-title').innerText = 'Login';
-        document.getElementById('open-modal-maker').innerText = '값을 입력하시오';
+        document.getElementById('open-modal-maker').innerText = '값을 입력하시오. ';
+        let span1 = document.createElement('span');
+        let span2 = document.createElement('span');
+        let labelID = document.createElement('label');
+        let labelPW = document.createElement('label');
+        let labelDesc = document.createElement('label');
         let idInput = makeInputTag('text', 'id', 'id');
         let pwInput = makeInputTag('password', 'pw', 'pw');
-        modalTmp.appendChild(idInput);
-        modalTmp.appendChild(pwInput);
+        labelID.innerText = "ID";
+        labelPW.innerText = "PW";
+        labelID.setAttribute('for', 'id');
+        labelID.setAttribute('for', 'pw');
+        labelDesc.setAttribute('id', 'labelDesc');
+        span1.setAttribute('class', 'searchbox');
+        span2.setAttribute('class', 'searchbox');
+        span1.style.height = '48px';
+        span2.style.height = '48px';
+        document.getElementById('open-modal-maker').appendChild(labelDesc);
+        modalTmp.appendChild(span1).appendChild(labelID);
+        span1.appendChild(idInput);
+        modalTmp.appendChild(document.createElement('br'));
+        modalTmp.appendChild(span2).appendChild(labelPW);
+        span2.appendChild(pwInput);
+        modalTmp.appendChild(document.createElement('br'));
         let submit = document.createElement('button');
         submit.innerText = '제출';
+        submit.setAttribute('class', 'my-btn hover4');
         submit.addEventListener('click', () => submitFetch('login', idInput, pwInput));
         let registerBtn = document.createElement('button');
+        registerBtn.setAttribute('class', 'my-btn hover4');
+        registerBtn.style.marginLeft = '1%';
+        registerBtn.style.marginRight = '1%';
         registerBtn.innerText = '회원 가입';
         registerBtn.addEventListener('click', () => submitFetch('register', idInput, pwInput));
         let findBtn = document.createElement('button');
         findBtn.innerText = '비번 찾기';
+        findBtn.setAttribute('class', 'my-btn hover4');
         findBtn.addEventListener('click', e => {
             modalClear();
         });
--- WebContent/assets/js/utils.js
+++ WebContent/assets/js/utils.js
@@ -3,6 +3,8 @@ const LOCAL_HOST = "http://127.0.0.1:8080";
 document.addEventListener('DOMContentLoaded', function () {
     let loginBtn = document.getElementById('login-btn');
     if (isLogin('JSESSIONID')) loginBtn.innerText = "Hello!";
+
+    startTypo();
 });
 
 function isLogin(cookieName) {
@@ -35,6 +37,7 @@ function modalClear() {
 function makeInputTag(type, id, name) {
     let ret = document.createElement('input');
     ret.setAttribute('type', type);
+    ret.setAttribute('class', 'input_text');
     ret.setAttribute('id', id);
     ret.setAttribute('name', name);
     return ret;
@@ -60,6 +63,77 @@ function submitFetch(action, idInput, pwInput) {
                 location.href = '#';
             } else {
                 // 모달 UI 변환(재로그인)
+                let labelDesc = document.getElementById('labelDesc');
+                labelDesc.style.color = 'red';
+                labelDesc.innerText = " ID 혹은 비밀번호를 확인해 주세요";
             }
         });
+}
+
+function startTypo() {
+
+    let str = "MADE-BY-";
+    let charlist = ["SSAFY...", "1ilsang...", "YoonJin..."];
+    let cl_order = 0;
+    let c = 0;
+
+    setInterval(function () {
+        let isvisible = document.getElementById('cursor').style.visibility;
+        if (isvisible === 'hidden') {
+            document.getElementById('cursor').style.visibility = 'visible'
+        } else {
+            document.getElementById('cursor').style.visibility = 'hidden'
+        }
+    }, 200);
+
+    let firstInterval = setInterval(function () {
+        if (c < 8) {
+            document.getElementById('cursor').style.paddingLeft = (document.getElementById('typing').offsetWidth + 60) + 'px';
+            document.getElementById('typing').innerText += str[c];
+            c++;
+        }
+        if (c === 8) {
+            let bflag = 1;
+            c = 0;
+            clearInterval(firstInterval);
+            (function () {
+                let flag = 0;
+                setInterval(function () {
+                    if (bflag === 1) {
+                        if (flag === 0) {
+                            document.getElementById('cursor').style.paddingLeft = (document.getElementById('typing').offsetWidth + 17) + 'px';
+                            if (c < charlist[cl_order].length) {
+                                document.getElementById('typing').innerText += charlist[cl_order][c];
+                                c++;
+                            }
+                            if (c === charlist[cl_order].length) {
+                                flag = 1;
+                            }
+                        }
+                        if (flag === 1) {
+                            if (c >= 0) {
+                                let buf = str;
+                                for (let i = 0; i < c; i++) {
+                                    buf += charlist[cl_order][i];
+                                }
+                                document.getElementById('typing').innerText = buf;
+                                document.getElementById('cursor').style.paddingLeft = (document.getElementById('typing').offsetWidth - 20) + 'px';
+                                c--;
+                            }
+                            if (c === -1) {
+                                c++;
+                                flag = 0;
+                                if (cl_order === 2) {
+                                    cl_order = 0;
+                                } else {
+                                    cl_order++;
+                                }
+                            }
+                        }
+                    }
+                }, 500);
+            })();
+        }
+    }, 500);
+
 }
